c8bb450d1e3427d17657b838c55d7c3f544c9c71
Ognjen Kecina <ognjenkecina@gmail.com>
2023-07-07 11:26:56 +0200

    Created login page

--- front/app/utils/RootNavigator.js
+++ front/app/utils/RootNavigator.js
@@ -1,1 +1,43 @@
+import React, {useEffect} from "react";
+import {StackActions, CommonActions} from "@react-navigation/native";
+import {BackHandler} from "react-native";
+
 export const navigationRef = React.createRef(null);
+
+export const navigate = (name, params) => {
+    if (navigationRef.current) {
+        navigationRef.current.navigate(name, params);
+    }
+};
+
+export const navigateCloseCurrent = (name, params) => {
+    if (navigationRef.current) {
+        navigationRef.current.dispatch(StackActions.replace(name, params))
+    }
+};
+
+export const navigateCloseAll = (name, params) => {
+    if (navigationRef.current) {
+        navigationRef.current.dispatch(
+            CommonActions.reset({
+                index: 0,
+                routes: [
+                    {
+                        name: name,
+                        params: params
+                    },
+                ],
+            })
+        )
+    }
+};
+
+export const goToPreviousScreen = () => {
+    if (navigationRef.current) {
+        navigationRef.current.dispatch(StackActions.pop())
+    }
+};
+
+export const canGoBack = () => {
+    return navigationRef.current && navigationRef.current.canGoBack();
+}
