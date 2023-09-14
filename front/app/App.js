bc4b659aa1a89c25e54113616246869f5fef58b7
Ognjen Kecina <ognjenkecina@gmail.com>
2023-09-14 12:46:51 +0200

    Updated hms push kit

--- front/app/App.js
+++ front/app/App.js
@@ -1,4 +1,13 @@
 import Navigator from "./src/routes/homeStack";
+import {useEffect} from "react";
+import {getToken, onRemoteMessageReceivedListener, setBackgroundMessageListener} from "./src/utils/huaweiService";
+
 export default function App() {
+    useEffect(() => {
+        getToken();
+        onRemoteMessageReceivedListener();
+        setBackgroundMessageListener();
+    }, []);
+
     return Navigator();
 }
